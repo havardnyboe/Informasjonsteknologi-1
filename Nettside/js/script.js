ec5e093f77274e68e602098a9b1f0ccea9acf594
Håvard Nybøe <havard.nyboe@gmail.com>
2020-10-05 21:24:47 +0200

    La til Nettside

---
+++ Nettside/js/script.js
@@ -0,0 +1,11 @@
+const gif = document.querySelector('#gif');
+const images = ['cat1', 'cat2', 'cat3', 'dance1', 'dance2', 'dance3', 'dog1', 'spiderman1', 'spiderman2'];
+
+function randImg() {
+    let i = Math.trunc(Math.random()*images.length)
+    gif.innerHTML = `<img src="img/${images[i]}.gif" height="400" alt="cat">`
+}
+
+window.onload = randImg
+document.querySelector('#gif-button').onclick = randImg
+document.querySelector('#gif-button-nav').onclick = randImg
