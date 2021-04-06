ef95b777ad8536ef71729f359223f3ff6fdca76e
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2021-04-06 15:27:56 +0200

    laget header

---
+++ 2021/11/Filmdatabase/template/header.php
@@ -0,0 +1,9 @@
+<nav class="menu-bar">
+    <span class="logo"><a href="/~haa0110/filmdatabase/index.php">!IMDb</a></span>
+    <ul>
+        <li><a href="/~haa0110/filmdatabase/php/filmer.php">Filmer</a></li>
+        <li><a href="/~haa0110/filmdatabase/php/serier.php">Serier</a></li>
+        <li><a href="/~haa0110/filmdatabase/php/boker.php">Bøker</a></li>
+        <li><a href="/~haa0110/filmdatabase/php/registrer.php">Legg til</a></li>
+    </ul>
+</nav>
