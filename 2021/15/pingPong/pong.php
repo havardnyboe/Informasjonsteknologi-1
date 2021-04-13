b1e1d0c0d1cdf5bb9701104f7b347822e6b399c3
Håvard Solberg Nybøe <40893697+havardnyboe@users.noreply.github.com>
2021-04-13 14:25:53 +0200

    pong

---
+++ 2021/15/pingPong/pong.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="nb">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pong</title>
+</head>
+
+<body style="background-color: teal;">
+    <h1 style="text-align: center; font-size: 100px;">Pong</h1>
+    <?php header( "refresh:0.8; url=index.php" ); ?>
+</body>
+
+</html>
