132858726d12bc481133a3c11ecf0902a7980815
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2021-04-15 00:05:50 +0200

    gjorde no sql greier tror jeg

--- 2021/11/Filmdatabase/php/registrer.php
+++ 2021/11/Filmdatabase/php/registrer.php
@@ -5,11 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap&family=Passion+One:wght@700&display=swap"
-        rel="stylesheet">
+    <?php include("../template/link.php") ?>
     <title>Filmdatabase - Registrer</title>
 </head>
 
